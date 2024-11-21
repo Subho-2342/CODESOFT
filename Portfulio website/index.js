da7d41478525562bd4b9f657ae9e4ef792730815
Subho-2342 <132153087+Subho-2342@users.noreply.github.com>
2024-11-21 21:27:37 +0530

    Add files via upload

---
+++ Portfulio website/index.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".text", {
+    strings: ["Web Developer", ],
+    typeSpeed: 100,
+    backSpeed: 100, // Corrected from 'backspeed' to 'backSpeed'
+    backDelay: 1000,
+    loop: true,
+  });
